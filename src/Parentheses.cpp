8362fdea8b2568bb8e5e4c71454f04f07c12157f
Piotr Hoffner <piotr.hoffner.wroc@gmail.com>
2019-08-12 19:26:44 +0200

    passed )( case

--- src/Parentheses.cpp
+++ src/Parentheses.cpp
@@ -1,8 +1,37 @@
 #include "Parentheses.hpp"
 
 bool Parentheses::isBalanced(std::string s) {
-    if(s.size()%2==0 or s.empty()) return true;
-    else false;
+    int nawias=0, kwadratowy=0, klamra=0;
+    for(auto it=s.begin(); it!=s.end(); ++it)
+    {
+        if(*it == '(') ++nawias;
+        if(*it == '[') ++kwadratowy;
+        if(*it == '{') ++klamra;
+        if(*it == ')') --nawias;
+        if(*it == ']') --kwadratowy;
+        if(*it == '}') --klamra;
+
+        if(nawias<0 || klamra<0 || kwadratowy<0) return false;
+    }
+
+    if(nawias==0 && kwadratowy==0 && klamra==0) return true;
+    else return false;   
+    
+    // auto pos1 = s.find('(');
+    // auto pos2 = s.find(')');
+
+    // if(s.size()%2==0 or s.empty())
+    // {
+    //     if(s[++pos1] !=')') {
+    //         return false;
+    //     } 
+
+    //     return true;
+    // } 
+    // else {
+    //     return false;
+    // } ;
+
     
     // if (s.empty()) return true;
     // bool open = s.find('(') != std::string::npos;
--- test_catch/ParenthesesTests.cpp
+++ test_catch/ParenthesesTests.cpp
@@ -83,5 +83,17 @@ SCENARIO("Parentheses", "[Parentheses]") {
         }
     }
 
+    GIVEN("Parentheses, )( input string") {
+        std::string input = ")(";
+        Parentheses p;
+
+        WHEN("isBalanced() is called") {
+            auto result = p.isBalanced(input);
+
+            THEN("The result is false") {
+                CHECK(result == false);
+            }
+        }
+    }
 
 }
