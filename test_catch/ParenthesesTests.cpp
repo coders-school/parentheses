431a402901762baa6fcf0d63a318dc5ef5e6433e
Marcin Bury <marcinburyit@gmail.com>
2019-08-12 18:20:19 +0200

    red )( zle

--- test_catch/ParenthesesTests.cpp
+++ test_catch/ParenthesesTests.cpp
@@ -65,6 +65,18 @@ SCENARIO("Parentheses", "[Parentheses]") {
                 }
             }
         }
+        GIVEN("Parentheses,)( input string") {
+            std::string input{")("};
+            Parentheses p;
+
+            WHEN("isBalanced() is called") {
+                auto result = p.isBalanced(input);
+
+                THEN("The result is false") {
+                    CHECK(result == false);
+                }
+            }
+        }
 }
 
 
