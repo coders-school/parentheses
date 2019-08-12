2b4ba93573d4f1afbf381401dfa868a4d4d98944
alicjaliQui <alicjaliqui@gmail.com>
2019-08-12 18:31:23 +0200

    )( ][ }{

--- src/Parentheses.cpp
+++ src/Parentheses.cpp
@@ -1,8 +1,10 @@
 #include "Parentheses.hpp"
 
 bool Parentheses::isBalanced(std::string s) {
-    if((s.size() % 2) != 0)
+    if((s.size() % 2) != 0)	    
         return false;
+    else if(s.front() == ')' or s.front() == ']' or s.front() == '}')
+	return false;
     else
         return true;
 }
