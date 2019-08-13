38ebbc2c52f160acfe1dbaf8055abe703504ef78
Ryszard Halapacz <rysiekh@gmail.com>
2019-08-13 11:00:45 +0200

    per aspera ad astra -stack

--- src/Parentheses.cpp
+++ src/Parentheses.cpp
@@ -1,6 +1,47 @@
 #include "Parentheses.hpp"
+#include <stack>
 
 bool Parentheses::isBalanced(std::string s) {
    
-   return (s!=")(");
+   std::stack < char > myStack;
+   
+   for(auto it = s.begin();it!=s.end();it++)
+   {
+      if(myStack.empty()==true)
+      {
+         myStack.push(*it);
+         continue;
+      } else if(myStack.top()=='(')
+      {
+         if((*it)!=')') 
+         {
+            myStack.push(*it);
+         }else
+         {
+            myStack.pop();
+         }
+      }else if(myStack.top()=='[')
+      {
+          if((*it)!=']') 
+         {
+            myStack.push(*it);
+         }else
+         {
+            myStack.pop();
+         }
+      }else if(myStack.top()=='{')
+      {
+         if((*it)!='}') 
+         {
+            myStack.push(*it);
+         }else
+         {
+            myStack.pop();
+         }
+      };
+   };
+   if(myStack.empty()) return true;
+   else return false;
+
+   
 }
