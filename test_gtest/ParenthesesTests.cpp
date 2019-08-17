b8fe1208ea525da442706fe15f0ae0859cd92088
teojdb <joasia.bednarska90@gmail.com>
2019-08-17 21:18:23 +0200

    Simple gtests

--- test_gtest/ParenthesesTests.cpp
+++ test_gtest/ParenthesesTests.cpp
@@ -14,3 +14,42 @@ TEST(ParenthesesTests, givenEmptyStringWhenIsBalancedIsCalledThenResultIsTrue)
     // THEN
     ASSERT_TRUE(result);
 }
+
+TEST(ParenthesesTests, givenCorrectTwoParentheses)
+{
+    // GIVEN
+    std::string input{"()"};
+    Parentheses p;
+
+    // WHEN
+     auto result = p.isBalanced(input);
+
+    // THEN
+    ASSERT_TRUE(result);
+}
+
+TEST(ParenthesesTests, givenOnlyOpenParentheses)
+{
+    // GIVEN
+    std::string input{"("};
+    Parentheses p;
+
+    // WHEN
+     auto result = p.isBalanced(input);
+
+    // THEN
+    ASSERT_FALSE(result);
+}
+
+TEST(ParenthesesTests, givenOtherVariant1)
+{
+    // GIVEN
+    std::string input{"[{]}"};
+    Parentheses p;
+
+    // WHEN
+     auto result = p.isBalanced(input);
+
+    // THEN
+    ASSERT_FALSE(result);
+}
