42909af81bf77b60376b4a382559b5675247c9ba
teojdb <joasia.bednarska90@gmail.com>
2019-08-17 20:53:15 +0200

    Refactor - changes in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
-#include "Parentheses.hpp"
+#include "../inc/Parentheses.hpp"
 
 int main()
 {
     std::cout << "Parentheses" << std::endl;
+    std::cout << "Insert paratheses string: " << std::endl;
+    std::string inp;
+    std::cin >> inp;
+    Parentheses p;
+    if (p.isBalanced(inp) == true){std::cout << "Parantheses string is balanced \n";}
+    else std::cout << "Parantheses string is NOT balanced" << std::endl;
     return 0;
 }
